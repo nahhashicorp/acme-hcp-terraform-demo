a55a3456e97b2f5e4a1a1dbad0fec2e21c1e8865
nahhashicorp <nicole.hargrove1@ibm.com>
2026-01-30 20:11:56 -0500

    Create variables.tf

---
+++ modules/ec2_app_server/variables.tf
@@ -0,0 +1,15 @@
+variable "name_prefix" { type = string }
+variable "environment" { type = string }
+
+variable "vpc_id"    { type = string }
+variable "subnet_id" { type = string }
+
+variable "allowed_ssh_cidr" { type = string }
+
+variable "instance_type" { type = string }
+variable "ami_id"        { type = string }
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
