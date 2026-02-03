a5032dfdfb567d08a3ae0cf1e15b0374a5fe70f1
nahhashicorp <nicole.hargrove1@ibm.com>
2026-02-02 22:50:38 -0500

    Update variables.tf

--- modules/ec2_app_server/variables.tf
+++ modules/ec2_app_server/variables.tf
@@ -1,13 +1,23 @@
 variable "name_prefix" { type = string }
+default = null
+
 variable "environment" { type = string }
+default = null
 
 variable "vpc_id"    { type = string }
+default = null
+
 variable "subnet_id" { type = string }
+default = null
 
 variable "allowed_ssh_cidr" { type = string }
+default = null
 
 variable "instance_type" { type = string }
+default = null
+
 variable "ami_id"        { type = string }
+default = null
 
 variable "tags" {
   type    = map(string)
